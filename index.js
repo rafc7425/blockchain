b7e51a5df7e85813e59c95ead2c446074cf79734
rick <ricawashere95@gmail.com>
2021-11-03 00:12:26 -0600

    second session

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hola");
+import PKG from './package.json';
+import Block from './src/blockchain/block';
+
+const { name, version } = PKG;
+
+console.log(`${name} v${version}`);
+
+const block = new Block('a', 'b', 'c', 'd');
+console.log(block.toString());
